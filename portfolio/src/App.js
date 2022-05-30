41e06978a112045321bd94288211a5f9948b84c3
leandrochs <leandrochagask@gmail.com>
2022-05-30 19:31:08 -0300

    feat: adiciona novas rotas em App

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
+import Home from './pages/home/Home';
+import Project from './pages/project/Project';
+import Technology from './pages/technology/Technology';
+import Provider from './context/Provider';
 
 export default function App() {
   return (
+    <Provider>
       <Switch>
         <Route exact path='/' component={Home} />
-        <Route exact path='/projetos' component={Projects} />
+        <Route exact path='/projeto' component={Project} />
+        <Route exact path='/tecnologia' component={Technology} />
       </Switch>
+    </Provider>
   );
 }
