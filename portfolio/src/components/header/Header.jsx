43f2dd2df4e553a30a5d15018aadff7c3fc32f1b
leandrochs <leandrochagask@gmail.com>
2022-05-30 21:58:46 -0300

    feat: adiciona scroll para condicionar className

--- portfolio/src/components/header/Header.jsx
+++ portfolio/src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { Context } from '../../context';
 import { Link } from 'react-router-dom';
 import './header.css';
@@ -6,13 +6,27 @@ import './header.css';
 export default function Header(props) {
   const displayRef = useRef(null);
   const { setDisplayRef } = useContext(Context);
+  const [scrollTop, setScrollTop] = useState('Header-header-container-top');
 
   useEffect(() => {
     setDisplayRef(displayRef);
   }, [setDisplayRef]);
 
+  useEffect(() => {
+    const scrollListener = () => {
+      window.scrollY > 40
+        ? setScrollTop('Header-header-container')
+        : setScrollTop('Header-header-container-top');
+    };
+    window.addEventListener('scroll', scrollListener);
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    };
+  }, []);
+
   return (
-    <header ref={displayRef} className='Header-header-container'>
+    <header ref={displayRef} className={scrollTop}>
       <div className='Header-div-Link-container'>
         <Link to='/' className='Header-Link'>
           {props.name ? props.name : 'LEANDRO SILVA'}
