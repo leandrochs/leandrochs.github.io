7208c3e8fc53935449f019f0487e1579d7d924cf
leandrochs <leandrochagask@gmail.com>
2022-07-04 12:29:28 -0300

    feat: adiciona useEffect para setar useRef de display

--- portfolio/src/components/header/Header.jsx
+++ portfolio/src/components/header/Header.jsx
@@ -1,16 +1,9 @@
-import React, { useContext, useEffect, useRef, useState } from 'react';
-import { Context } from '../../context';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './header.css';
 
 export default function Header(props) {
-  const displayRef = useRef(null);
-  const { setDisplayRef } = useContext(Context);
   const [scrollTop, setScrollTop] = useState('Header-header-container-top');
-
-  useEffect(() => {
-    setDisplayRef(displayRef);
-  }, [setDisplayRef]);
 
   useEffect(() => {
     const scrollListener = () => {
@@ -26,7 +19,7 @@ export default function Header(props) {
   }, []);
 
   return (
-    <header ref={displayRef} className={scrollTop}>
+    <header className={scrollTop}>
       <div className='Header-div-Link-container'>
         <Link to='/' className='Header-Link'>
           {props.name ? props.name : 'LEANDRO SILVA'}
