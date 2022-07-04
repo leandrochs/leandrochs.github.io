8ac5d54098c41be3fcba63ff58f807f550c45d18
leandrochs <leandrochagask@gmail.com>
2022-07-04 12:46:03 -0300

    feat: adiciona descricao do projeto pixelart

--- portfolio/src/data/dataProjects.js
+++ portfolio/src/data/dataProjects.js
@@ -1,4 +1,5 @@
 const todoList = require('../images/projectsImages/todoList.png');
+const pixelArt = require('../images/projectsImages/pixelArt.png');
 
 const dataProjects = [
   {
@@ -10,6 +11,16 @@ const dataProjects = [
       'Projeto Todo List em html e lógica em Javascript.',
     deploy: 'https://leandrochs.github.io/Todo-List/',
     tags: ['#todo-list', '#html', '#javascript'],
+  },
+  {
+    type: 'Project',
+    name: 'PIXEL ART',
+    image: pixelArt,
+    subTitle: 'Projeto Pixel Art',
+    description:
+      'Projeto Pixel Art onde podemos ver a interação do usuário com elementos da tela por meio do DOM com html e Javascript.',
+    deploy: 'https://leandrochs.github.io/Pixels-Art/',
+    tags: ['#pixel-art', '#dom', '#javascript'],
   },
   
 ];
