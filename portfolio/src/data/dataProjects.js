8c3f00418b6a2990aef7e243d0198c1eb359db7b
leandrochs <leandrochagask@gmail.com>
2022-07-04 13:00:46 -0300

    feat: adiciona descricao do projeto mistery letter

--- portfolio/src/data/dataProjects.js
+++ portfolio/src/data/dataProjects.js
@@ -1,5 +1,6 @@
 const todoList = require('../images/projectsImages/todoList.png');
 const pixelArt = require('../images/projectsImages/pixelArt.png');
+const colorGuess = require('../images/projectsImages/colorGuess.png');
 
 const dataProjects = [
   {
@@ -21,6 +22,26 @@ const dataProjects = [
       'Projeto Pixel Art onde podemos ver a interação do usuário com elementos da tela por meio do DOM com html e Javascript.',
     deploy: 'https://leandrochs.github.io/Pixels-Art/',
     tags: ['#pixel-art', '#dom', '#javascript'],
+  },
+  {
+    type: 'Project',
+    name: 'COLOR GUESS',
+    image: colorGuess,
+    subTitle: 'Projeto Color Guess',
+    description:
+      'Projeto Color Guess em que temos um jogo de adivinhação de cores.',
+    deploy: 'https://leandrochs.github.io/Color-Guess/',
+    tags: ['#color-guess', '#jogo', '#javascript'],
+  },
+  {
+    type: 'Project',
+    name: 'MISTERY LETTER',
+    image: colorGuess,
+    subTitle: 'Projeto Mistery Letter',
+    description:
+      'Projeto Mistery Letter em que uma carta misteriosa é gerada com letras e fontes randômicas a partir de um texto digitado.',
+    deploy: 'https://leandrochs.github.io/Color-Guess/',
+    tags: ['#mistery-letter', '#html', '#javascript'],
   },
   
 ];
