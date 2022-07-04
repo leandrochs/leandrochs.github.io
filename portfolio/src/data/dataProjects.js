255b2c67452d1eae5e9940f119dbae02d8be44d9
leandrochs <leandrochagask@gmail.com>
2022-07-04 12:30:02 -0300

    feat: adiciona json para projetos

---
+++ portfolio/src/data/dataProjects.js
@@ -0,0 +1,16 @@
+const todoList = require('../images/projectsImages/todoList.png');
+
+const dataProjects = [
+  {
+    type: 'Project',
+    name: 'TODO LIST',
+    image: todoList,
+    subTitle: 'Projeto Todo List em html',
+    description:
+      'Projeto Todo List em html',
+    tags: ['#todo-list', '#html', '#javascript'],
+  },
+  
+];
+
+export default dataProjects;
