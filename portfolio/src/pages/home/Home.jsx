6c58bfc80844489ed3cf404402574fea0b36f7ec
leandrochs <leandrochagask@gmail.com>
2022-07-04 21:57:04 -0300

    refactor: adiciona carousel de projetos e remove links

--- portfolio/src/pages/home/Home.jsx
+++ portfolio/src/pages/home/Home.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import Carousel from '../../components/carrossel/Carousel';
 import Display from '../../components/display/Display';
 import Header from '../../components/header/Header';
@@ -7,7 +6,7 @@ import { Context } from '../../context';
 import './home.css';
 
 export default function Home() {
-  const { setDisplayName } = useContext(Context);
+  const { displayName, setDisplayName } = useContext(Context);
 
   useEffect(() => {
     setDisplayName('LEANDRO SILVA');
@@ -15,18 +14,13 @@ export default function Home() {
 
   return (
     <div className='Home-div-container'>
-      <Header name={'DEVELOPER'} />
+      <Header name={displayName} />
 
       <Display />
 
-      <Carousel type={'technology'} />
+      <Carousel title={'TECNOLOGIAS'} type={'technology'} />
 
-      <div>
-        <Link to='/projeto'>PROJETOS</Link>
-      </div>
-      <div>
-        <Link to='/tecnologia'>TECNOLOGIA</Link>
-      </div>
+      <Carousel title={'PROJETOS'} type={'project'} />
     </div>
   );
 }
